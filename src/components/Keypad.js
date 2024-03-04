e5a76af4d266421b0b1b9366ca2ac3bcaa3e21be
hsanch13 <hayleysanchez13@gmail.com>
2024-03-03 21:38:11 -0700

    wrote an onClick function in Keypad and created a component with onFocus and onBlur functions

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+import EyesOnMe from "./EyesOnMe"
 
 function Keypad (){
+    function handleChange(){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} />
+        </div>
     )
 }
 
